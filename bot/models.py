69f3dd2824c56b2b84577edeee73ee25eb97cee2
MashaKereb <mashakereb@gmail.com>
2016-07-19 12:25:38 +0300

    pep update

--- bot/models.py
+++ bot/models.py
@@ -2,16 +2,14 @@ from django.db import models
 
 
 class Question(models.Model):
-	question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
 
-	def __str__(self):
-		return self.question_text
+    def __str__(self):
+        return self.question_text
+
 
 class ChatHistory(models.Model):
     answers = models.TextField()
-    answers_number = models.SmallIntegerField(default = 0)
-    
-    
 
 
-
+answers_number = models.SmallIntegerField(default=0)
